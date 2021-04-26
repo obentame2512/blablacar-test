f288073a3fe58f8b8874c2dadd7ae46446fc7abf
obentame2512 <83086736+obentame2512@users.noreply.github.com>
2021-04-26 17:54:29 +0200

    Update dim_currency.py

--- dim_currency.py
+++ dim_currency.py
@@ -1,20 +1,27 @@
 import pandas
 
-def create_dim_cur(table):
-    dates = table.drop([0,1,2,3,4])
-    dates = dates.melt(id_vars=['Titre :'], var_name= 'cur_code', value_name= 'one_euro_value')
-    dates = dates.rename(columns = {'Titre :': 'cur_date'})
-    dates = dates[(dates.one_euro_value != '-') & (dates.one_euro_value.notnull())]
-    dates['cur_date']= pandas.to_datetime(dates['cur_date'], format='%d/%m/%Y')
-    dates_max = dates[['cur_code', 'cur_date']]
-    dates_max = dates_max.groupby('cur_code').max()
-    dates = pandas.merge(dates, dates_max, 'inner', ['cur_code', 'cur_date'])
-    dates = dates.rename(columns = {'cur_date': 'last_updated_date'})
+# Create the table dim_currency with all available currencies in the file
+def dim_cur(table):
+    cur_history = table.drop([0,1,2,3,4])
+    # Create a table cur_history with the history of the currencies and their values in euro
+    cur_history = cur_history.melt(id_vars=['Titre :'], var_name= 'cur_code', value_name= 'one_euro_value')
+    cur_history = cur_history.rename(columns = {'Titre :': 'cur_date'})
+    cur_history = cur_history[(cur_history.one_euro_value != '-') & (cur_history.one_euro_value.notnull())]
+    cur_history['cur_date']= pandas.to_datetime(cur_history['cur_date'], format='%d/%m/%Y')
+    # Calculate the last update date and value of each currency
+    last_update = cur_history[['cur_code', 'cur_date']]
+    last_update = last_update.groupby('cur_code').max()
+    last_update_currency = pandas.merge(cur_history, last_update, 'inner', ['cur_code', 'cur_date'])
+    last_update_currency = last_update_currency.rename(columns = {'cur_date': 'last_updated_date'})
+    # Create a table ser_code with the currencies and their serial codes
     ser_code = pandas.DataFrame(table.iloc[0:1,1:])
     ser_code = ser_code.melt(var_name= 'cur_code', value_name= 'Serial_code')
-    dim_currency = pandas.merge(ser_code, dates, 'inner', ['cur_code'])
+    # merge ser_code and last_update_currency
+    dim_currency = pandas.merge(ser_code, last_update_currency, 'inner', ['cur_code'])
     dim_currency['cur_code'] = dim_currency['cur_code'].str.rsplit('(', 1).str[1].str.rsplit(')', 1).str[0]
     return dim_currency
   
- df = pandas.read_csv('http://webstat.banque-france.fr/fr/downloadFile.do?id=5385698&exportType=csv', delimiter= ';')
-currency_date = create_dim_cur(df)
+df = pandas.read_csv('http://webstat.banque-france.fr/fr/downloadFile.do?id=5385698&exportType=csv', delimiter= ';')
+ # apply the function dim_cur to the table df
+dim_currency = dim_cur(df)
+
